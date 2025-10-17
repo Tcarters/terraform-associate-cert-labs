c770a4d88d70f2c61e3185c80f8b1dd2af197aea
Tcarters <71230412+Tcarters@users.noreply.github.com>
2025-10-17 23:31:10 +0200

    Update outputs.tf

--- 10_tf_sensitive_variables/outputs.tf
+++ 10_tf_sensitive_variables/outputs.tf
@@ -1,5 +1,5 @@
 
-output "instance_ids" {
+output "instance_ids" {  # Instance ID output
     description     = "IP address of Database instance"
     value           = aws_db_instance.database.address
 }
